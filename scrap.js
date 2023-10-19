2290a1efdd8fd9d7c0d9c62e797609339413f75f
gillianbc <gszemeti@gmail.com>
2023-10-19 06:58:48 +0100

    WIP on memoize

--- scrap.js
+++ scrap.js
@@ -1,69 +1,143 @@
 console.log('This is the scrap file')
 
 /*
-Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+Given a function fn, return a memoized version of that function.
 
-The first time the returned function is called, it should return the same result as fn.
-Every subsequent time it is called, it should return undefined.
+A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+You can assume there are 3 possible input functions: sum, fibonacci, and factorial.
+
+sum accepts two integers a and b and returns a + b.
+fibonacci accepts a single integer n and returns 1 if n <= 1 or fibonacci(n - 1) + fibonacci(n - 2) otherwise.
+factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
 
 
 Example 1:
 
-Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
-Output: [{"calls":1,"value":6}]
+Input:
+fnName = "sum"
+actions = ["call","call","getCallCount","call","getCallCount"]
+values = [[2,2],[2,2],[],[1,2],[]]
+Output: [4,4,1,3,2]
 Explanation:
-const onceFn = once(fn);
-onceFn(1, 2, 3); // 6
-onceFn(2, 3, 6); // undefined, fn was not called
+const sum = (a, b) => a + b;
+const memoizedSum = memoize(sum);
+memoizedSum(2, 2); // "call" - returns 4. sum() was called as (2, 2) was not seen before.
+memoizedSum(2, 2); // "call" - returns 4. However sum() was not called because the same inputs were seen before.
+// "getCallCount" - total call count: 1
+memoizedSum(1, 2); // "call" - returns 3. sum() was called as (1, 2) was not seen before.
+// "getCallCount" - total call count: 2
 Example 2:
 
-Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
-Output: [{"calls":1,"value":140}]
+Input:
+fnName = "factorial"
+actions = ["call","call","call","getCallCount","call","getCallCount"]
+values = [[2],[3],[2],[],[3],[]]
+Output: [2,6,2,2,6,2]
 Explanation:
-const onceFn = once(fn);
-onceFn(5, 7, 4); // 140
-onceFn(2, 3, 6); // undefined, fn was not called
-onceFn(4, 6, 8); // undefined, fn was not called
+const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+const memoFactorial = memoize(factorial);
+memoFactorial(2); // "call" - returns 2.
+memoFactorial(3); // "call" - returns 6.
+memoFactorial(2); // "call" - returns 2. However factorial was not called because 2 was seen before.
+// "getCallCount" - total call count: 2
+memoFactorial(3); // "call" - returns 6. However factorial was not called because 3 was seen before.
+// "getCallCount" - total call count: 2
+Example 3:
+
+Input:
+fnName = "fibonacci"
+actions = ["call","getCallCount"]
+values = [[5],[]]
+Output: [8,1]
+Explanation:
+fibonacci(5) = 8 // "call"
+// "getCallCount" - total call count: 1
 
 
 Constraints:
 
-calls is a valid JSON array
-1 <= calls.length <= 10
-1 <= calls[i].length <= 100
-2 <= JSON.stringify(calls).length <= 1000
-
+0 <= a, b <= 105
+1 <= n <= 10
+0 <= actions.length <= 105
+actions.length === values.length
+actions[i] is one of "call" and "getCallCount"
+fnName is one of "sum", "factorial" and "fibonacci"
  */
 
-const once = (fn) => {
-    let used = false;
+const memoize = (fn) => {
+    console.log(fn.toString());
 
-    return function(...args) {
-        if (!used) {
-            used = true
-            return fn(...args)
+    let cache = {};
+
+    const isCached = (func, argVals) => {
+        // Does the cache contain a request for this function with these args?
+        console.log('Checking the cache ' + func + ' ' + argVals)
+        // TODO
+        return false;
+    }
+
+    function addNewCacheFunction(functionName, functionArgs, result) {
+        // add a new entry to the cache for this function
+        let argsArray = [functionArgs]
+        let resultsArray = [ result ]
+        let cacheEntry = {};
+        let cacheValue = { args: argsArray, results: resultsArray}
+        cacheEntry[functionName] = cacheValue;
+        Object.assign(cache, cacheEntry);
+    }
+
+    function appendToExistingCacheFunction(existingCacheEntryForFunc, functionArgs, result) {
+        existingCacheEntryForFunc.args.push(functionArgs);
+        existingCacheEntryForFunc.results.push(result)
+    }
+
+    const addToCache = (func, argVals) => {
+        // adds the function, args and result to the cache
+        console.log('Adding to the cache ' + func + ' ' + argVals)
+        let functionName = func.name;
+        let result = func(...argVals)
+
+        // get the existing cache row for this function
+        let existingCacheEntryForFunc = cache[functionName];
+        let functionArgs = Array.from(argVals);
+
+        if (existingCacheEntryForFunc != undefined) {
+            appendToExistingCacheFunction(existingCacheEntryForFunc, functionArgs, result);
+        } else {
+
+            addNewCacheFunction(functionName, functionArgs, result);
         }
-        else {
-            return undefined;
+
+        console.log('Cache after add ' + JSON.stringify(cache))
+
+    }
+
+    const getCachedValue = (func, argVals) => {
+        console.log('Getting from the cache ' + func + ' ' + argVals)
+        let row = cache[func];
+        // TODO - how do I find the entry for this set of args?  forEach?
+        return undefined;
+    }
+
+    return function (...args) {
+        if (!isCached(fn, args)) {
+            console.log('Not from the cache')
+            addToCache(fn, args)
+            return fn(...args)
+        } else {
+            console.log('From the cache')
+            return getCachedValue(fn, ...args);
         }
     }
 }
 
 
-let fn = (a,b,c) => (a + b + c)
-console.log(fn(1,2,3))
-console.log(fn(2,4,3))
-
-const onceFn = once(fn);
-console.log(onceFn.toString())
-console.log("First " + onceFn(1,2,3))
-console.log("Second " + onceFn(2,4,3))
+const sum = (a, b, c) => a + b + c;
+const memoizedSum = memoize(sum);
+const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+const fibonacci = (n) => (n <= 1) ? 1 : (fibonacci(n - 1) + fibonacci(n - 2));
 
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+console.log(memoizedSum(1, 2, 3))
+console.log(memoizedSum(12, 2, 3))
