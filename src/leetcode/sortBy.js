0881a0a03e9c98569347d384ac37fef044458543
gillianbc <gszemeti@gmail.com>
2023-10-29 20:20:57 +0000

    2724 sortBy

--- src/leetcode/sortBy.js
+++ src/leetcode/sortBy.js
@@ -1,4 +1,5 @@
 /**
+ * 2724 SortBy
  * Given an array arr and a function fn, return a sorted array sortedArr.
  * You can assume fn only returns numbers and those numbers determine the sort order of sortedArr.
  * sortedArray must be sorted in ascending order by fn output.
